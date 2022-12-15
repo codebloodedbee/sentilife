0775812e0e5dd35bccc83344e2ccc49eb64174fb
hizkeel <dhizkeel@gmail.com>
2022-12-15 07:30:22 +0100

    updat

--- app/src/main/java/com/sapphirevirtual/sentilife/EditProfile.java
+++ app/src/main/java/com/sapphirevirtual/sentilife/EditProfile.java
@@ -3,6 +3,7 @@ package com.sapphirevirtual.sentilife;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class EditProfile extends AppCompatActivity {
 
@@ -11,5 +12,8 @@ public class EditProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
         getSupportActionBar().hide();
+    }
+
+    public void submit(View view) {
     }
 }
